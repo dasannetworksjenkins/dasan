ce746e7dcfc3981ad72989b70fa1f87ec3db8425
cicdjenkins1 <cicd.jenkins1@gmail.com>
2021-09-23 17:19:56 +0900

    add fail case

--- whattotest.cpp
+++ whattotest.cpp
@@ -1,12 +1,8 @@
 // whattotest.cpp
-#include <math.h>
+#include <stdio.h>
 
-double squareRoot(const double a) {
-    double b = sqrt(a);
-    if(b != b) { // nan check
-        return -1.0;
-    }else{
-        return sqrt(a);
-    }
+int main(void){
+	printf("gtest_jenkins_test\n");
+	return 0;
+
 }
-
---
+++ whattotest.h
@@ -0,0 +1,12 @@
+// whattotest.h
+#include <math.h>
+
+double squareRoot(const double a) {
+    double b = sqrt(a);
+    if(b != b) { // nan check
+        return -1.0;
+    }else{
+        return sqrt(a);
+    }
+}
+
